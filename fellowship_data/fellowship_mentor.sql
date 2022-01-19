053aed3e637e6f9d16d12bb6d3048b3cbe794e98
Kanchan Jaiswal <91010576+kanchanCode@users.noreply.github.com>
2022-01-19 09:11:39 +0530

    [Kanchan]Add:fellowship_mentor_table

---
+++ fellowship_data/fellowship_mentor.sql
@@ -0,0 +1,6 @@
+use normalization;
+CREATE TABLE fellowship_mentor (
+mentor_id int(11) NOT NULL PRIMARY KEY ,
+mentor_name varchar(100) NOT NULL,
+lab_no int NOT NULL,
+work_status varchar(20) NOT NULL)
